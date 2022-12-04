8b7dec8042d7111d8b9cc33d8f3b928372c4fd94
David Sotto Mayor <ddsmmf@gmail.com>
2022-12-03 23:41:48 -0400

    Adding cors headers and adding a feature in getUsers function controller

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const routes = require('./routes/github-routes')
 const app = express();
 
+// Setting the CORS headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Access-Methods', 'GET, POST, PATCH, PUT, DELETE');
+    res.setHeader('Access-Control-Access-Headers', 'Content-Type, Authorization');
+    next();
+})
+
 app.use(routes)
 
 app.listen(8000)
--- controllers/github-controller.js
+++ controllers/github-controller.js
@@ -15,10 +15,12 @@ exports.getUsers = async (req, res, next) => {
     const { data } = await axios.get(
         `${baseURL}/users?since=${since}&per_page=${itemsPerPage}`
     );
+    const firstRecordId = data[0].id
     const lastRecordId = data[itemsPerPage - 1].id;
 
     return res.status(200).json({
         users: JSON.stringify(data),
+        previous_page: `/api/users?since=${firstRecordId - 1}`,
         next_page: `/api/users?since=${lastRecordId}`,
     });
 };
